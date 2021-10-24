d75faa5b400741288704a45aa1fbe0d4d69385fe
MariaRamos1295 <mariabramos95@gmail.com>
2021-10-24 11:22:55 -0500

    subiendo

---
+++ src/test/java/Caso1.java
@@ -0,0 +1,112 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.awt.*;
+
+
+public class Caso1 {
+    public static void  main(String [] args ) {
+        System.setProperty("webdriver.chrome.driver", "src\\Driver\\chromedriver.exe");
+
+        //Iniciar el webdriver
+        WebDriver driver = new ChromeDriver();
+
+        //Abrir el sitio web
+        driver.get("http://automationpractice.com/index.php");
+
+        //Maximizar ventana
+        driver.manage().window().maximize();
+
+        //Credenciales
+        String username = "mariabramos95@gmail.com";
+        String password = "123456789";
+
+        //Dar clic en el boton Sign In
+        WebElement SignInBoton = driver.findElement(By.className("login"));
+        SignInBoton.click();
+
+        //Ingresar Credenciales De Inicio Sesion
+        WebElement emailInput = driver.findElement(By.id("email"));
+        WebElement passwordInput = driver.findElement(By.id("passwd"));
+        WebElement SignBoton = driver.findElement(By.id("SubmitLogin"));
+
+        emailInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        SignBoton.click();
+
+        //-----------Proceso de compra--------------------//
+
+        //ir pagina de inicio
+        WebElement logo = driver.findElement(By.id("header_logo"));
+        logo.click();
+
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+
+        js.executeScript("window.scrollBy(0,525)");
+
+        //Colocar el cursor en el producto
+        WebElement cursor =driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[1]/div/div[1]/div"));
+
+        Actions action = new Actions(driver);
+        action.moveToElement(cursor).perform();
+
+        //Agregar al Carro de compras
+        WebElement addtocart = driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[1]/div/div[2]/div[2]/a[2]"));
+        addtocart.click();
+
+        //continuar con la compra
+        js.executeScript("window.scrollBy(0,150)");
+        WebElement layer = driver.findElement(By.name("Submit"));
+        layer.click();
+        driver.navigate().refresh();
+
+        WebElement carro = driver.findElement(By.className("shopping_cart"));
+        carro.click();
+
+        WebElement continuar1 = driver.findElement(By.xpath("//*[@id=\"center_column\"]/p[2]/a[1]"));
+        continuar1.click();
+
+        js.executeScript("window.scrollBy(0,500)");
+        WebElement continuar2 = driver.findElement(By.name("processAddress"));
+        continuar2.click();
+
+        js.executeScript("window.scrollBy(0,500)");
+
+        WebElement terminosycondiciones = driver.findElement(By.name("cgv"));
+        terminosycondiciones.click();
+
+        js.executeScript("window.scrollBy(0,500)");
+
+        WebElement processcarrier = driver.findElement(By.name("processCarrier"));
+        processcarrier.click();
+
+        js.executeScript("window.scrollBy(0,500)");
+
+        WebElement pago = driver.findElement(By.xpath("//*[@id=\"HOOK_PAYMENT\"]/div[1]/div"));
+        pago.click();
+
+        js.executeScript("window.scrollBy(0,500)");
+
+        WebElement confirm = driver.findElement(By.xpath("//*[@id=\"cart_navigation\"]/button"));
+        confirm.click();
+        js.executeScript("window.scrollBy(0,200)");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/test/java/Caso2_5.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Caso2_5 {
+    public static void  main(String [] args ) {
+        System.setProperty("webdriver.chrome.driver", "src\\Driver\\chromedriver.exe");
+
+        //Iniciar el webdriver
+        WebDriver driver = new ChromeDriver();
+
+        //Abrir el sitio web
+        driver.get("http://automationpractice.com/index.php");
+
+        //Maximizar ventana
+        driver.manage().window().maximize();
+
+        //Colocar el cursor en el producto
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+
+        js.executeScript("window.scrollBy(0,650)");
+
+        WebElement cursor =driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[1]/div/div[1]/div"));
+        Actions action = new Actions(driver);
+        action.moveToElement(cursor).perform();
+        cursor.click();
+    }
+}
